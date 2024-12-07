23769588cfdc249dd09673a2441555a5128b7bca
Rafael Franco <rafasole84@outlook.com.br>
2024-12-07 12:21:12 -0300

    Delete heranca-prototipo3-funcao-prototype.js

--- heranca-prototipo3-funcao-prototype.js
+++
@@ -1,7 +0,0 @@
-//criando uma propriedade e associando direto a função construtora
-
-function User(){};
-User.prototype.perfil = 'estudante'; //definindo que "perfil" é uma propriedade de user
-let estudante = new User() //criando um novo usuário com nome estudante
-
-console.log(estudante.perfil)
